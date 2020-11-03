9f55be4212bfd59e460d1d7d086a9487d334fa71
smudgecat123 <jf14218@my.bristol.ac.uk>
2020-11-03 01:46:05 +0000

    added triplanar textures back into the shader

--- project_blue/scenes/main/SimpleTerrain.gd
+++ project_blue/scenes/main/SimpleTerrain.gd
@@ -83,12 +83,12 @@ func update_arrays():
 	var vertex_num: int = pow(resolution + 1, 2)
 	var cell_num: int = pow(resolution, 2)
 	var cell_size: float = 1/float(resolution)
-	
+
 	#get noise, add positions and colors per vertex
 	for y in range(0, resolution + 1):
 		for x in range(0, resolution + 1):
 			var raw_noise_val = (noise.get_noise_2d(x * cell_size * scale_period, y * cell_size * scale_period) + 1)/2
-			
+
 			var island_scale = 1
 			if island_mesh:
 				var radius = resolution / 2
@@ -100,49 +100,49 @@ func update_arrays():
 					island_scale = 1 - ((radius_scale - island_start_radius)/(1-island_start_radius))
 				else:
 					island_scale = 0
-			
+
 			var noise_val = height_curve.interpolate_baked(raw_noise_val) * scale_y * island_scale
 			var index = x + y * (resolution + 1)
 			var c = 0.5 if center_mesh else 0
 			vertices[index] = Vector3(x * cell_size - c, noise_val, y * cell_size - c) * scale_xyz
 			colors[index] = Color.from_hsv(fposmod(raw_noise_val, 1),1,1)
-	
+
 	#add normals and indices per cell
 	for y in range(0, resolution):
 		for x in range(0, resolution):
-			
+
 			#get cell indices
 			var cell_index = x + y * resolution
 			var index_index = cell_index * 6
-			
+
 			#get point indices
 			var top_left = x + y * (resolution + 1)
 			var top_right = (x+1) + y * (resolution + 1)
 			var bottom_left = x + (y+1) * (resolution + 1)
 			var bottom_right = (x+1) + (y+1) * (resolution + 1)
-			
+
 			#first triangle indices
 			indices[index_index] = top_left
 			indices[index_index + 1] = bottom_right
 			indices[index_index + 2] = bottom_left
-			
+
 			#second triangle indices
 			indices[index_index + 3] = bottom_right
 			indices[index_index + 4] = top_left
 			indices[index_index + 5] = top_right
-			
+
 			#first triangle normals
 			var normal1: Vector3 = get_normal(vertices[top_left], vertices[bottom_right], vertices[bottom_left])
 			normals[top_left] += normal1
 			normals[bottom_right] += normal1
 			normals[bottom_left] += normal1
-			
+
 			#second triangle normals
 			var normal2: Vector3 = get_normal(vertices[bottom_right], vertices[top_left], vertices[top_right])
 			normals[bottom_right] += normal2
 			normals[top_left] += normal2
 			normals[top_right] += normal2
-	
+
 	#normalize normals
 	for i in range(0, vertex_num):
 		normals[i] = normals[i].normalized()
@@ -159,7 +159,7 @@ func resize_arrays():
 		normals.resize(vertex_num)
 		var cell_num: int = pow(resolution, 2)
 		indices.resize(cell_num * 6)
-	
+
 func create_arrays():
 	if vertices == null: vertices = PoolVector3Array()
 	if colors == null: colors = PoolColorArray()
